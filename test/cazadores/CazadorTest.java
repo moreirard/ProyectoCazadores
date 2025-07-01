64ca2c7eb26a1dc4ae5c7534da3ebc9a8cbb9b4d
Darío Moreira <moreirard@gmail.com>
2025-07-01 20:09:50 -0300

    Se agrega primer test case

---
+++ test/cazadores/CazadorTest.java
@@ -0,0 +1,14 @@
+package cazadores;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CazadorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
